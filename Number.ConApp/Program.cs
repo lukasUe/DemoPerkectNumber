38e762b9f050c6dc8bc4dfb1d77efb5ad2b03d16
lukasUe <luki182@gmail.com>
2021-07-06 18:27:27 +0200

    Add project files.

---
+++ Number.ConApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Number.ConApp
+{
+  class Program
+  {
+#pragma warning disable IDE0060 // Remove unused parameter
+    static void Main(string[] args)
+#pragma warning restore IDE0060 // Remove unused parameter
+    {
+      Console.WriteLine("Number-App!");
+
+      string input,output;
+      long number;
+      //Eingabe (E)
+      do
+      {
+        Console.Write("Geben Sie eine Zahl ein:");
+        input = Console.ReadLine();
+
+      } while (long.TryParse(input, out number) == false);
+
+      //Verarbeitung (V)
+      if (Number.Logic.NumberProperties.IsPerfect(number))
+      {
+        output = $"{number} ist eine perfekte Zahl";
+      }
+      else
+      {
+        output = $"{number} ist keine perfekte Zahl";
+      }
+      //Ausgabe (A)
+      Console.WriteLine(output);
+
+    }
+  }
+}
